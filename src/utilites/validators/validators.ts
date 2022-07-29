d3d1b27554b787df8241736634c0ccaee245ba50
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-07-29 17:08:48 +0200

    Added Redux Toolkit and Basic Settings to the Project

--- src/utilites/validators/validators.ts
+++
@@ -1,9 +0,0 @@
-export type ValidatorType = (values: string) => string | undefined
-
-export const notEmpty: ValidatorType = values => values.length ? undefined : "Please, type something before submiting"
-
-export const maxLengthCreator = (maxLength: number): ValidatorType => values => {
-  return values.length > maxLength
-    ? `Max length is ${ maxLength } symbols`
-    : undefined
-}
