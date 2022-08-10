5525521f4ed1f80494562294fc3ff40fee819acd
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-08-10 12:04:03 +0200

    Added Animation when added new item to the cart

--- src/components/PizzaBlock/PizzaBlock.tsx
+++ src/components/PizzaBlock/PizzaBlock.tsx
@@ -24,6 +24,7 @@ const PizzaBlock: FC<IPizza> = ({
                                 }) => {
   const [activeType, setActiveType] = useState(0)
   const [activeSize, setActiveSize] = useState(0)
+  const [isClicked, setIsClicked] = useState(false)
 
   const pizzaTypes = ['Original', 'Thin']
   const pizzaSizes = [25, 30, 35]
@@ -33,6 +34,8 @@ const PizzaBlock: FC<IPizza> = ({
 
 
   const onAddClick = () => {
+    setIsClicked(true)
+    setTimeout(() => setIsClicked(false), 1000)
     dispatch(addPizza({
       id,
       title,
@@ -41,7 +44,6 @@ const PizzaBlock: FC<IPizza> = ({
       size: pizzaSizes[activeSize],
       type: pizzaTypes[activeType]
     }))
-
   }
 
   return (
@@ -69,19 +71,29 @@ const PizzaBlock: FC<IPizza> = ({
             ) }
           </ul>
         </div>
-        <div className=" button button--outline button--add" onClick={ onAddClick }>
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white" />
-          </svg>
-          <span>Add To Cart For <span className="pizza-block__price" style={ {
-            position: 'relative',
-            top: '1px',
-            fontSize: '18px',
-            fontWeight: 'bold'
-          } }>${ currentPrice }</span></span>
-        </div>
+        <button
+          className={ !isClicked
+            ? 'button button--outline button--add'
+            : 'button button--added' }
+          disabled={ isClicked }
+          onClick={ onAddClick }>
+          {
+            isClicked
+              ? <>
+                <span className="cross" />
+                <span>Added To Cart</span>
+              </>
+              : <>
+                <span className="cross" />
+                <span>Add To Cart For <span className="pizza-block__price" style={ {
+                  position: 'relative',
+                  top: '1px',
+                  fontSize: '18px',
+                  fontWeight: 'bold'
+                } }>${ currentPrice }</span></span>
+              </>
+          }
+        </button>
       </div>
     </div>
   )
